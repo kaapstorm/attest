4dfbdf853e29583fd3fd201590b8f5291f11f0bf
Norman Hooper <nhooper@dimagi.com>
2025-11-01 23:31:13 +0000

    Replace optparse with argparse

--- attest/run.py
+++ attest/run.py
@@ -1,79 +1,101 @@
-import sys
+import argparse
 import os
+import sys
+from importlib.metadata import version
 from os import path
 
-from importlib.metadata import version
-from optparse import OptionParser, make_option
 from attest.collectors import Tests
+from attest.hook import AssertImportHook
 from attest.reporters import get_all_reporters, get_reporter_by_name
 from attest.utils import parse_options
-from attest.hook import AssertImportHook
 
 
 def make_parser(**kwargs):
-    args = dict(
-        prog='attest',
-        usage='%prog [options] [tests...] [key=value...]',
-        version=version('attest'),
-
-        description=(
+    parser_kwargs = {
+        'prog': 'attest',
+        'usage': '%(prog)s [options] [tests...] [key=value...]',
+        'description': (
             'The positional "tests" are dotted '
             'names for modules or packages that are scanned '
             'recursively for Tests instances, or dotted names '
             'for any other object that iterates over tests. If '
             'not provided, packages in the working directory '
-            'are scanned.\n'
+            'are scanned. '
             'The key/value pairs are passed to the '
             'reporter constructor, after some command-line '
             'friendly parsing.'
         ),
+    }
+    parser_kwargs.update(kwargs)
+    parser = argparse.ArgumentParser(**parser_kwargs)
 
-        option_list=[
-            make_option('-d', '--debugger',
-                action='store_true',
-                help='enter pdb for failing tests',
-            ),
-            make_option('-r', '--reporter',
-                metavar='NAME',
-                help='select reporter by name'
-            ),
-            make_option('-l', '--list-reporters',
-                action='store_true',
-                help='list available reporters'
-            ),
-            make_option('-n', '--no-capture',
-                action='store_true',
-                help="don't capture stderr and stdout"
-            ),
-            make_option('--full-tracebacks',
-                action='store_true',
-                help="don't clean tracebacks"
-            ),
-            make_option('--fail-fast',
-                action='store_true',
-                help='stop at first failure'
-            ),
-            make_option('--native-assert',
-                action='store_true',
-                help="don't hook the assert statement"
-            ),
-            make_option('-p', '--profile',
-                metavar='FILENAME',
-                help='enable tests profiling and store results in filename'
-            ),
-            make_option('-k', '--keyboard-interrupt',
-                action='store_true',
-                help="Let KeyboardInterrupt exceptions (CTRL+C) propagate"
-            ),
-        ]
+    # Add version argument
+    parser.add_argument(
+        '--version',
+        action='version',
+        version=f"%(prog)s {version('attest')}"
     )
-    args.update(kwargs)
-    return OptionParser(**args)
+
+    # Add optional arguments
+    parser.add_argument(
+        '-d', '--debugger',
+        action='store_true',
+        help='enter pdb for failing tests',
+    )
+    parser.add_argument(
+        '-r', '--reporter',
+        metavar='NAME',
+        help='select reporter by name'
+    )
+    parser.add_argument(
+        '-l', '--list-reporters',
+        action='store_true',
+        help='list available reporters'
+    )
+    parser.add_argument(
+        '-n', '--no-capture',
+        action='store_true',
+        help="don't capture stderr and stdout"
+    )
+    parser.add_argument(
+        '--full-tracebacks',
+        action='store_true',
+        help="don't clean tracebacks"
+    )
+    parser.add_argument(
+        '--fail-fast',
+        action='store_true',
+        help='stop at first failure'
+    )
+    parser.add_argument(
+        '--native-assert',
+        action='store_true',
+        help="don't hook the assert statement"
+    )
+    parser.add_argument(
+        '-p', '--profile',
+        metavar='FILENAME',
+        help='enable tests profiling and store results in filename'
+    )
+    parser.add_argument(
+        '-k', '--keyboard-interrupt',
+        action='store_true',
+        help="Let KeyboardInterrupt exceptions (CTRL+C) propagate"
+    )
+
+    # Add positional arguments (tests and key=value pairs)
+    parser.add_argument(
+        'args',
+        nargs='*',
+        help='test modules/packages and key=value options'
+    )
+
+    return parser
 
 
 def main(tests=None, **kwargs):
     parser = make_parser(**kwargs)
-    options, args = parser.parse_args()
+    args = parser.parse_args()
 
     # When run as a console script (i.e. ``attest``), the CWD isn't
     # ``sys.path[0]``, but it should be. It's important to do this early in
@@ -83,35 +105,38 @@ def main(tests=None, **kwargs):
     if sys.path[0] not in ('', cwd):
         sys.path.insert(0, cwd)
 
-    if options.list_reporters:
+    if args.list_reporters:
         for reporter in get_all_reporters():
             print(reporter)
         return
 
-    opts = parse_options(args)
-    reporter = get_reporter_by_name(options.reporter)(**opts)
+    opts = parse_options(args.args)
+    reporter = get_reporter_by_name(args.reporter)(**opts)
 
     if not tests:
-        names = [arg for arg in args if '=' not in arg]
+        names = [arg for arg in args.args if '=' not in arg]
         if not names:
             names = [name for name in os.listdir('.')
                           if path.isfile(f'{name}/__init__.py')]
 
-        if options.native_assert:
+        if args.native_assert:
             tests = Tests(names)
         else:
             with AssertImportHook():
                 tests = Tests(names)
 
     def run():
-        tests.run(reporter, full_tracebacks=options.full_tracebacks,
-                            fail_fast=options.fail_fast,
-                            debugger=options.debugger,
-                            no_capture=options.no_capture,
-                            keyboard_interrupt=options.keyboard_interrupt)
+        tests.run(
+            reporter,
+            full_tracebacks=args.full_tracebacks,
+            fail_fast=args.fail_fast,
+            debugger=args.debugger,
+            no_capture=args.no_capture,
+            keyboard_interrupt=args.keyboard_interrupt,
+        )
 
-    if options.profile:
-        filename = options.profile
+    if args.profile:
+        filename = args.profile
         import cProfile
         cProfile.runctx('run()', globals(), locals(), filename)
         print(f'Wrote profiling results to {filename!r}.')
